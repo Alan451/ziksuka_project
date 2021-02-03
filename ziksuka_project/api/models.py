8127ba676278c21ce51c186f5517b89553e9313a
Alan <alantthomas451@gmail.com>
2021-02-03 20:19:35 +0530

    created comments model

--- ziksuka_project/api/models.py
+++ ziksuka_project/api/models.py
@@ -6,3 +6,7 @@ class Blog(models.Model):
     title = models.CharField(max_length=100,default='title')
     content = models.CharField(max_length=1000,default='content')
 
+class Comment(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    blog = models.ForeignKey(Blog,on_delete=models.CASCADE)
+    comment = models.CharField(max_length=200,default='comment')
